d4662c816142f384b18c2f9ab192f48d82732e98
Facundo Martinez Correa <fnmartinez88@gmail.com>
2012-06-18 00:51:01 -0300

    parser touched

---
+++ parser/c/gr-parser.c
@@ -0,0 +1,322 @@
+#include "gr-parser.h"
+char * grammar_name;
+
+static char * terminals;
+static int term_size;
+static int term_qty;
+static char * non_terminals;
+int nterm_size;
+int nterm_qty;
+static char distinguished;
+static prod_list productions;
+int valid_name, valid_grammar, valid_nt, valid_t, valid_d, valid_p;
+
+int exists(char * s, char c, size_t l) {
+	int i;
+	for (i = 0; i < l && s[i] != c; i++)
+		;
+	return i < l;
+}
+
+prod_list new_prod_list() {
+	prod_list pl = malloc(sizeof(prod_list_t));
+	pl->size = 0;
+	pl->head = NULL;
+	pl->tail = NULL;
+	return pl;
+}
+
+prod new_prod(char left, char * right) {
+	prod p = malloc(sizeof(prod_t));
+	p->left = left;
+	p->right= right;
+	return p;
+}
+
+int add_to_list(prod_list pl, prod p) {
+	if (pl == NULL || p == NULL) {
+		return 0;
+	}
+
+	if (pl->size == 0) {
+		pl->head = p;
+		pl->tail = p;
+		p->next = p->prev = NULL;
+	} else {
+		pl->tail->next = p;
+		p->prev = pl->tail;
+		pl->tail = p;
+		p->next = NULL;
+	}
+	pl->size++;
+	return 1;
+}
+
+prod pop_prod(prod_list pl) {
+	if (pl == NULL || pl->size == 0) {
+		return NULL;
+	}
+
+	prod p = pl->head;
+	pl->head = pl->head->next;
+	if (pl->head == NULL) {
+		pl->tail = NULL;
+	} else {
+		pl->head->prev = NULL;
+	}
+	pl->size--;
+	return p;
+}
+
+void destroy_prod(prod p) {
+	if (p == NULL) {
+		return;
+	}
+	free(p->right);
+	free(p);
+}
+
+void destroy_prod_list(prod_list pl) {
+	if (pl == NULL) {
+		return;
+	}
+	if (pl->size != 0) {
+		while (pl->size > 0) {
+			destroy_prod(pop_prod(pl));
+		}
+	}
+	free(pl);
+}
+
+void add_terminal(char * s) {
+	int i = 0;
+	int l = strlen(s);
+	for (i = 0; i < l && isspace(s[i]); i++)
+		;
+
+	if (isupper(s[i])) {
+		printf(
+				"Error: Invalid terminal. All terminal symbols must be lowercase letters.\n");
+		exit(1);
+	}
+
+	if (!exists(terminals, s[i], term_qty)) {
+		if (term_size == term_qty) {
+			char * aux;
+			aux = realloc(terminals, term_size *= 1.5);
+			if (aux == NULL) {
+				printf("Error: Not enought memory!\n");
+				exit(1);
+			}
+			terminals = aux;
+		}
+		terminals[term_qty++] = s[i];
+	} else {
+		printf("Error: Repeated terminal symbols encountered.\n");
+		exit(1);
+	}
+}
+
+void add_non_terminal(char * s) {
+	int i = 0;
+	int l = strlen(s);
+	for (i = 0; i < l && isspace(s[i]); i++)
+		;
+
+	if (islower(s[i])) {
+		printf(
+				"Error: Invalid non_terminal. All terminal symbol must be uppercase letters.\n");
+		exit(1);
+	}
+
+	if (!exists(non_terminals, s[i], nterm_qty)) {
+		if (nterm_size == nterm_qty) {
+			char * aux;
+			aux = realloc(non_terminals, term_size *= 1.5);
+			if (aux == NULL) {
+				printf("Error: Not enought memory!\n");
+				exit(1);
+			}
+			non_terminals = aux;
+		}
+		non_terminals[nterm_qty++] = s[i];
+	} else {
+		printf("Error: Repeated non terminal symbol encountered.\n");
+		exit(1);
+	}
+}
+
+void add_production2(char le, char * s) {
+	int i, j, k;
+	for (i = 0; s[i] != '\0' && isspace(s[i]); i++);
+	for (j = i; s[j] != '\0' && !(isspace(s[j]) || s[j] != ',' || s[j] != '|' || s[j] != '}' ); j++);
+
+	if( i-j == 1 && s[j] != LAMDA){
+		printf("1Error: Invalid right-side production symbol\n");
+		exit(1);
+	}
+
+	for (k = i; k <= j; k++) {
+		if ((isupper(s[k]) && !exists(terminals, s[k], term_qty))
+				|| (islower(s[k]) && !exists(non_terminals, s[k], nterm_qty))) {
+			printf("1Error: Invalid right-side production symbol\n");
+			exit(1);
+
+		}
+	}
+
+	char * r = malloc(sizeof(char) * (j-i + 1));
+	memcpy(r, s+i, j-i);
+	r[j-1+1] = '\0';
+
+	add_to_list(productions, new_prod(le, r));
+}
+
+void add_production(char * s) {
+	int i;
+	int l = strlen(s);
+	for (i = 0; i < l && isspace(s[i]); i++)
+		;
+	char le = s[i];
+	if (islower(le) || !exists(non_terminals, s[i], nterm_qty)) {
+		printf("Error: Invalid left-side production symbol\n");
+		exit(1);
+	}
+	for (i = i + 1; i < l && isspace(s[i]); i++)
+		;
+	for (i = i + 1; i < l && isspace(s[i]); i++)
+		;
+	add_production2(le, s + i + 1);
+
+}
+
+void set_distinguished(char d){
+	distinguished = d;
+}
+
+void init() {
+	term_size = INIT_QTY;
+	term_qty = 0;
+	terminals = malloc(term_size * sizeof(char));
+	nterm_size = INIT_QTY;
+	nterm_qty = 0;
+	non_terminals = malloc(nterm_size * sizeof(char));
+	productions = new_prod_list();
+	valid_name = false;
+	valid_grammar = false;
+	valid_nt = false;
+	valid_t = false;
+	valid_d = false;
+	valid_p = false;
+	atexit(finale);
+}
+
+void printall() {
+	int i;
+
+	printf("G = <{");
+	for (i = 0; i < nterm_qty; i++) {
+		printf("%c%c ", non_terminals[i], (i == nterm_qty - 1 ? ' ' : ','));
+	}
+	printf("},{");
+	for (i = 0; i < term_qty; i++) {
+		printf("%c%c ", terminals[i], (i == term_qty - 1 ? ' ' : ','));
+	}
+	printf("}, ");
+	printf("%c, ", distinguished);
+
+	prod p;
+	for (p = productions->head; p != NULL; p = p->next) {
+		printf("%c -> %c%c, ", p->left, p->right[0], p->right[1]);
+	}
+	printf("}>\n");
+}
+
+void check_gr() {
+	prod p = NULL;
+	for(p = productions->head; p != NULL; p=p->next){
+		if(!is_unitary(p)){
+
+		}
+	}
+}
+
+int is_unitary(prod p){
+	return is_non_terminal(p->right[0]) && p->right[1] == LAMDA;
+}
+
+void process() {
+	if (!valid_name) {
+		printf("Syntax Error: Not a valid grammar identifier\n");
+		exit(1);
+	}
+	if (!valid_nt) {
+		printf("Syntax Error: Valid non-terminal symbols not found\n");
+		exit(1);
+	}
+	if (!valid_t) {
+		printf("Syntax Error: Valid terminals symbols not found\n");
+		exit(1);
+	}
+	if (!valid_d) {
+		printf("Syntax Error: Valid distinguished element not found\n");
+		exit(1);
+	}
+	if (!valid_p) {
+		printf("Syntax Error: Valid productions not found\n");
+		exit(1);
+	}
+	if (!valid_grammar) {
+		printf("Syntax Error: Malformed grammar\n");
+		exit(1);
+	}
+
+	check_gr();
+	
+	printf("Grammar: \n Terminals: ");
+	int i;
+	for(i=0; i<term_qty; i++){
+		printf("%c", terminals[i]);
+	}
+	printf("\nNon-Terminals: ");
+	for(i=0; i<term_qty; i++){
+			printf("%c ", non_terminals[i]);
+	}
+	printf("\nDistinguished: %c\n", distinguished);
+	printf("Productions:\n");
+	prod_t * p;
+	for(p = productions->head; p != NULL; p = p->next){
+		printf("%c -> %s", p->left, p->right);
+	}
+
+//	GrammarADT gr = newGrammar();
+//
+//	setTerminals(gr, terminals, term_qty);
+//	setNonTerminals(gr, non_terminals, nterm_qty);
+//	setDistinguished(gr, distinguished);
+//
+//	ProductionsADT ps = newProductions(0);
+//
+//	prod p = NULL;
+//	for (p = productions->head; p != NULL; p = p->next) {
+//		addProduction(ps, newProduction(p->left, p->right[0], p->right[1]));
+//	}
+//
+//	setProductions(gr, ps);
+//
+//	printf("Grammar before Conversion\n");
+//	printGrammar(gr);
+//
+//	formalize(gr);
+//	generateASDR(gr);
+//
+//	printf("Grammar after Conversion\n");
+//	printGrammar(gr);
+
+}
+
+void finale(void){
+	destroy_prod_list(productions);
+	free(terminals);
+	free(non_terminals);
+}
---
+++ parser/c/gr-parser.h
@@ -0,0 +1,68 @@
+#ifndef __GR_PARSER__
+#define __GR_PARSER__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+//#include "../../include/Converter.h"
+//#include "../../include/Grammar.h"
+//#include "../../include/Automata.h"
+
+#ifndef LAMDA
+#define LAMDA '\\'
+#endif
+#define RIGHT 1
+#define NONE 0
+#define LEFT -1
+#define INIT_QTY 10
+#define is_terminal(X) (islower((X)) || (X) == LAMDA)
+#define is_non_terminal(X) isupper((X))
+#define validate_name() valid_name=true
+#define validate_grammar() valid_grammar=true
+#define validate_non_terminals() valid_nt=true
+#define validate_terminals() valid_t=true
+#define validate_productions() valid_p=true
+#define validate_distinguished() valid_d=true
+
+typedef struct prod_t prod_t;
+typedef struct prod_list_t prod_list_t;
+typedef prod_t * prod;
+typedef prod_list_t * prod_list;
+
+enum{
+	false,
+	true
+} bool;
+
+struct prod_t{
+	prod prev;
+	prod next;
+	char left;
+	char * right;
+};
+struct prod_list_t{
+	prod head;
+	prod tail;
+	size_t size;
+};
+
+void init(void);
+void finale(void);
+void process(void);
+void printall(void);
+void set_distinguished(char d);
+void add_production(char * s);
+void add_production2(char le, char * s);
+void add_non_terminal(char * s);
+void add_terminal(char * s);
+void destroy_prod_list(prod_list pl);
+void destroy_prod(prod p);
+prod pop_prod(prod_list pl);
+int add_to_list(prod_list pl, prod p);
+prod new_prod(char left, char* right);
+prod_list new_prod_list();
+int exists(char * s, char c, size_t l);
+int is_unitary(prod p);
+void check_gr(void);
+#endif
